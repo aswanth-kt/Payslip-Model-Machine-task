033b994024101311bae08910aea17b8eeebb0054
aswanthkt31 <aswanthkt99@gmail.com>
2026-04-01 00:33:43 +0530

    fix: add missing css file

--- frontend/src/page/GeneratePayslip.jsx
+++ frontend/src/page/GeneratePayslip.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
 import "./GeneratePayslip.css";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 
